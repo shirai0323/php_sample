6c11635817be6fc17c25ca1ec4cd9c9547d37ac2
shirai0323 <a.shirai0323@gmail.com>
2023-05-04 21:14:39 +0900

    [Add]route設定

--- php_sample/routes/web.php
+++ php_sample/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/task', function (Request $request) {
+
+});
+
+Route::delete('/task', function () {
+
+});
