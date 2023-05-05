dde9e2ddc15bea60f59bda64fe52204a1597285c
shirai0323 <a.shirai0323@gmail.com>
2023-05-05 10:06:33 +0900

    [Add]view追加

---
+++ php_sample/resources/views/tasks.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Basic Tasks</title>
+ <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+ <div class="container">
+   <h3 class="my-3">タスク管理ツール</h3>
+   <div class="card mb-3">
+     <div class="card-header">タスク新規追加</div>
+     <div class="card-body">
+       <form method="POST" action="{{ url('/task') }}">
+         @csrf
+         <div class="form-group">
+           <input type="text" name="name" class="form-control">
+           @if ($errors->has('name'))
+           <p class="text-danger">{{ $errors->first('name') }}</p>
+           @endif
+           <button type="submit" class="btn btn-outline-info mt-2"><i class="fas fa-plus fa-lg mr-2"></i>追加</button>
+         </div>
+       </form>
+     </div>
+   </div>
+ </div>
+</body>
+</html>
--- php_sample/routes/web.php
+++ php_sample/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
 });
 
 Route::post('/task', function (Request $request) {
