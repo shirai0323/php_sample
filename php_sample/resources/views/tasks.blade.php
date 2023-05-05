12c699703b42801068a3641681acdbb1b7d5237e
shirai0323 <a.shirai0323@gmail.com>
2023-05-05 11:09:45 +0900

    [Fix]Bootstrapデザイン適用修正

--- php_sample/resources/views/tasks.blade.php
+++ php_sample/resources/views/tasks.blade.php
@@ -3,6 +3,7 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ @vite(['resources/sass/app.scss', 'resources/js/app.js'])
  <title>Basic Tasks</title>
  <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
